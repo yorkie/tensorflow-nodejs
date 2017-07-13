e348f832bd1d2fa31b9a29bdeb59840f5b4b67a4
yorkie <yorkiefixer@gmail.com>
2017-07-13 19:44:33 +0800

    improve apis

--- mnist.js
+++ mnist.js
@@ -3,16 +3,16 @@
 const tf = require('./');
 const graph = tf.createGraph();
 
-const x = graph.placeholder(tf.dtype.int32);
-const y = graph.const(30, [1]);
-const z = graph.addN([x, y]);
+const x = graph.placeholder();
+const y = graph.const(30);
+const z = graph.add(x, y);
 
 // const W = graph.variable(graph.zeros([784, 10]));
 // const b = graph.variable(graph.zeros([10]));
 // const y = graph.nn.softmax(graph.matmul(x, W) + b);
 
 const session = tf.createSession(graph);
-const feeds = tf.Tensor.from(1000, false, [1]);
+const feeds = tf.Tensor.from(1000);
 const res = session.run(z, [x, feeds]);
 
 console.log('result is:', res);
