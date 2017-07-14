392c1ca31a48b23f285da26bbd23ce71411663c9
yorkie <yorkiefixer@gmail.com>
2017-07-14 18:23:11 +0800

    improve some ops

--- mnist.js
+++ mnist.js
@@ -3,14 +3,17 @@
 const tf = require('./');
 const graph = tf.createGraph();
 
-const x = graph.placeholder();
-const y = graph.const(30);
-const z = graph.add(x, y);
+// const x = graph.placeholder();
+// const y = graph.const(30);
+// const z = graph.add(x, y);
+
+const x = graph.const([5, 103], 3, [100]);
+const z = graph.zeros(x);
 
 // const W = graph.variable(graph.zeros([784, 10]));
 // const b = graph.variable(graph.zeros([10]));
 // const y = graph.nn.softmax(graph.matmul(x, W) + b);
 
 const session = tf.createSession(graph);
-const res = session.run(z, 1000);
+const res = session.run(z);
 console.log('result is:', res);
