897f1f6ba413caf0fccfed335ae7427a26b488e5
yorkie <yorkiefixer@gmail.com>
2017-07-19 19:48:07 +0800

    add train directory

---
+++ test/test_op_training.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+const tf = require('../');
+const graph = tf.graph();
+const session = tf.session();
+
+const loss = graph.const([100, 20], 3, [2]);
+new tf.train.GradientDescentOptimizer(0.5).minimize(loss)
