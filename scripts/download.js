0861d40078023bc4578cd0cfbfa3c9748eadff7e
yorkie <yorkiefixer@gmail.com>
2017-07-27 15:26:19 +0800

    build: download protobuf files before installing

--- scripts/download.js
+++ scripts/download.js
@@ -5,12 +5,15 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const tar = require('tar-fs');
+const unzip = require('unzip');
 const gunzip = require('gunzip-maybe');
 
 const version = '1.2.1';
 const TF_TYPE = process.env.TF_TYPE || 'cpu';
 const DOWNLOAD_URL = 'https://storage.googleapis.com/tensorflow/libtensorflow/' +
   `libtensorflow-${TF_TYPE}-${os.platform()}-x86_64-${version}.tar.gz`;
+const PROTOBUF_URL = 'https://storage.googleapis.com/tensorflow/libtensorflow/' +
+  `libtensorflow_proto-${version}.zip`;
 
 https.get(DOWNLOAD_URL, (res) => {
   if (res.statusCode !== 200) {
@@ -19,4 +22,15 @@ https.get(DOWNLOAD_URL, (res) => {
     console.log(DOWNLOAD_URL + ' is finished downloaded.');
   }
   res.pipe(gunzip()).pipe(tar.extract('./tensorflow'));
+});
+
+https.get(PROTOBUF_URL, (res) => {
+  if (res.statusCode !== 200) {
+    throw new Error(PROTOBUF_URL + ' ' + res.statusMessage);
+  } else {
+    console.log(PROTOBUF_URL + ' is finished downloaded.');
+  }
+  res.pipe(unzip.Extract({
+    path: './protobuf'
+  }));
 });
